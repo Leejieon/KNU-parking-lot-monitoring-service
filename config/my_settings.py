afaaf4108d4cb1162a99d50af7dec8c613f8e5ef
wonjunJ <95316036+wonjunJ@users.noreply.github.com>
2023-05-02 22:26:38 +0900

    jun3 commit

---
+++ config/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'knu_pklot',
+        'USER': 'root',
+        'PASSWORD': 'ben0722',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
